a058b0a8ad491edcb7268b4503bdd96c68f6963a
Овчаренко Борис <akinfeev-34@mail.ru>
2021-03-10 19:34:18 +0300

    Изменения структуры

---
+++ FireForecasting/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MathCore.WPF.ViewModels;
+
+namespace FireForecasting.ViewModels
+{
+    class MainWindowViewModel:ViewModel
+    {
+        private string _Title = "Главное окно программы";
+
+        public string Title { get=>_Title; set => Set(ref _Title,value); }
+    }
+}
---
+++ FireForecasting/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FireForecasting.ViewModels
+{
+    class ViewModelLocator
+    {
+    }
+}
