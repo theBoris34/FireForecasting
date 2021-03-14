0518dc4892322bb1e11972cc5e39ebd5cc3ad347
Овчаренко Борис <akinfeev-34@mail.ru>
2021-03-14 13:28:39 +0300

    Конфигурация БД в проекте. DbRegistrator.

---
+++ FireForecasting/Data/DbRegistrator.cs
@@ -0,0 +1,32 @@
+﻿using FireForecasting.DAL.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FireForecasting.Data
+{
+    static class DbRegistrator
+    {
+        public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration Configuration) => services
+            .AddDbContext<DepartmentDB>(opt =>
+            {
+                var type = Configuration["Type"];
+                switch (type)
+                {
+                    case null: throw new InvalidOperationException("Не определен тип БД");
+                    default: throw new InvalidOperationException($"Тип подключения {type} не поддерживается");
+
+                    case "MSSQL":
+                        opt.UseSqlServer(Configuration.GetConnectionString(type));
+                        break;
+                    case "InMemory":
+                        opt.UseInMemoryDatabase("FireForecastingDB");
+                        break;
+                }
+            })
+            ;
+    }
+}
