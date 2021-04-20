960e3426abe30e95916db003cf5f3f48db7b1815
Овчаренко Борис <akinfeev-34@mail.ru>
2021-04-20 11:58:44 +0300

    Модель ПА.

--- FireForecasting.DAL/Entityes/Departments/FireTruck.cs
+++ FireForecasting.DAL/Entityes/Departments/FireTruck.cs
@@ -10,7 +10,7 @@ namespace FireForecasting.DAL.Entityes.Departments
     /// </summary>
     class FireTruck:Entity
     {
-      
+        #region Свойства
         /// <summary>
         /// Тип ПА (АЦ,АЛ и тп.)
         /// </summary>
@@ -77,6 +77,111 @@ namespace FireForecasting.DAL.Entityes.Departments
         /// Высота подъема стрелы - м.
         /// </summary>
         public int LiftingHeight { get; set; }
+        #endregion
+
+        public FireTruck() : this("None", "None",DateTime.Now,1,60,50)
+        {
+
+        }
+
+        /// <summary>
+        /// Создание экземпляра Пожарного автомобиля.
+        /// </summary>
+        /// <param name="brand">Производитель автомобиля.</param>
+        /// <param name="model">Модель автомобиля.</param>
+        /// <param name="yearOfCreation">Год выпуска.</param>
+        /// <param name="numberOfSeats">Количество мест.</param>
+        /// <param name="maxSpeed">Максимальная скорость.</param>
+        /// <param name="fuelVolume">Объем бензобака.</param>
+        public FireTruck(string brand, string model, DateTime yearOfCreation, byte numberOfSeats, byte maxSpeed, int fuelVolume)
+        {         
+
+            if (string.IsNullOrEmpty(brand))
+            {
+                throw new ArgumentException("Производитель ПА не может быть пустым или иметь значение null", nameof(brand));
+            }
+
+            if (string.IsNullOrEmpty(model))
+            {
+                throw new ArgumentException("Модель ПА не может быть пустым или иметь значение null", nameof(model));
+            }
+
+            Brand = brand;
+            Model = model;
+            YearOfCreation = yearOfCreation;
+            NumberOfSeats = numberOfSeats;
+            MaxSpeed = maxSpeed;
+            FuelVolume = fuelVolume;
+        }
+
+        /// <summary>
+        /// Создание экземпляра Пожарного автомобиля с цистерной.
+        /// </summary>
+        /// <param name="brand">Производитель автомобиля.</param>
+        /// <param name="model">Модель автомобиля.</param>
+        /// <param name="yearOfCreation">Год выпуска.</param>
+        /// <param name="numberOfSeats">Количество мест.</param>
+        /// <param name="maxSpeed">Максимальная скорость.</param>
+        /// <param name="fuelVolume">Объем бензобака.</param>
+        /// <param name="fireEngine">Модель пожарной установки.</param>
+        /// <param name="tankVolume">Объем цистерны.</param>
+        /// <param name="foamVolume">Объем пенообразователя.</param>
+        /// <param name="pumpCapacity">Производительность насоса.</param>
+        public FireTruck(string type, string brand, string model, DateTime yearOfCreation, byte numberOfSeats, byte maxSpeed, int fuelVolume, string fireEngine, int tankVolume, int foamVolume, double pumpCapacity) 
+            : this(brand, model, yearOfCreation, numberOfSeats, maxSpeed, fuelVolume)
+        {
+            if (string.IsNullOrEmpty(type))
+            {
+                throw new ArgumentException("Тип ПА не может быть пустым или иметь значение null", nameof(type));
+            }
+            FireEngine = fireEngine;
+            TankVolume = tankVolume;
+            FoamVolume = foamVolume;
+            PumpCapacity = pumpCapacity;
+        }
+
+        /// <summary>
+        /// Создание экземпляра Пожарного автомобиля с цистерной и лестницей.
+        /// </summary>
+        /// <param name="type">Тип Пожарного автомобиля.</param>
+        /// <param name="brand">Производитель автомобиля.</param>
+        /// <param name="model">Модель автомобиля.</param>
+        /// <param name="yearOfCreation">Год выпуска.</param>
+        /// <param name="numberOfSeats">Количество мест.</param>
+        /// <param name="maxSpeed">Максимальная скорость.</param>
+        /// <param name="fuelVolume">Объем бензобака.</param>
+        /// <param name="fireEngine">Модель пожарной установки.</param>
+        /// <param name="tankVolume">Объем цистерны.</param>
+        /// <param name="foamVolume">Объем пенообразователя.</param>
+        /// <param name="pumpCapacity">Производительность насоса.</param>
+        /// <param name="liftingHeight">Высота стрелы.</param>
+        public FireTruck(string type, string brand, string model, DateTime yearOfCreation, byte numberOfSeats, byte maxSpeed, int fuelVolume, string fireEngine, int tankVolume, int foamVolume, double pumpCapacity, int liftingHeight)
+            : this(type, brand, model, yearOfCreation, numberOfSeats, maxSpeed, fuelVolume, fireEngine, tankVolume, foamVolume, pumpCapacity)
+        {            
+            LiftingHeight = liftingHeight;
+        }
+
+        /// <summary>
+        /// Создание экземпляра Пожарного автомобиля с лестницей.
+        /// </summary>
+        /// <param name="type">Тип Пожарного автомобиля.</param>
+        /// <param name="brand">Производитель автомобиля.</param>
+        /// <param name="model">Модель автомобиля.</param>
+        /// <param name="yearOfCreation">Год выпуска.</param>
+        /// <param name="numberOfSeats">Количество мест.</param>
+        /// <param name="maxSpeed">Максимальная скорость.</param>
+        /// <param name="fuelVolume">Объем бензобака.</param>
+        /// <param name="liftingHeight">Высота стрелы.</param>
+        public FireTruck(string type, string brand, string model, DateTime yearOfCreation, byte numberOfSeats, byte maxSpeed, int fuelVolume, int liftingHeight)
+            : this(brand, model, yearOfCreation, numberOfSeats, maxSpeed, fuelVolume)
+        {
+            if (string.IsNullOrEmpty(type))
+            {
+                throw new ArgumentException("Тип ПА не может быть пустым или иметь значение null", nameof(type));
+            }
+            LiftingHeight = liftingHeight;
+        }
+
 
         public override string ToString()
         {
